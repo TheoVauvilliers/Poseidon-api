7f46932b5bf3cce90ff093a4b97e3419d0d0c4af
Théo Vauvilliers <theo.vauvilliers10@gmail.com>
2021-12-08 13:48:51 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import Fastify from 'fastify'
+// import mongoConnector from './config/db.js'
+// import { routes } from './endpoints/main.js'
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = Fastify({ logger: true })
+
+// app.register(mongoConnector)
+// app.register(routes)
+
+const start = async () => {
+    try {
+        await app.listen(process.env.SERVER_PORT)
+    } catch (err) {
+        app.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
